dc10c4d8ef920312640a9c1d84e6d5ead26f05ac
Antoine Ridé <aride@normandiewebschool.fr>
2021-10-25 14:01:03 +0200

    post

---
+++ index0.php
@@ -0,0 +1,35 @@
+<?php
+$memory ="";
+$comparaison = 1;
+$Tableau = array(9, 5, 6, 3, 2, 1, 4, 8, 7);
+echo "Donnée : ";
+for($i = 0; $i < count($Tableau); $i++) {
+ $memory++;
+ $comparaison++;
+ echo  $Tableau[$i].", ";
+ 
+}
+ 
+for($I = count($Tableau) - 2;$I >= 0; $I--) {
+ $comparaison++;
+ for($J = 0; $J <= $I; $J++) {
+  $comparaison++;
+  if($Tableau[$J + 1] < $Tableau[$J]) {
+   $comparaison++;
+   $t = $Tableau[$J + 1];
+   $Tableau[$J + 1] = $Tableau[$J];
+   $Tableau[$J] = $t;
+   $memory+= 6;
+   
+  }
+ }
+}
+
+echo "Affichage : ";
+for($j = 0; $j < count($Tableau); $j++) {
+  $comparaison++;
+  echo $Tableau[$j].", ";
+}
+echo "Mémoire : $memory";
+echo "Comparaison : $comparaison";
+?>
